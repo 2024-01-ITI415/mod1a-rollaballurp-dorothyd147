2cad2022ec2fda76c12bdda5eed88ab54e8ebe5a
dcdoan153 <123130804+dcdoan153@users.noreply.github.com>
2024-01-29 22:41:34 -0500

    Cameras Fixed
    Fixed angle of camera, and code for CameraController

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,22 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-    
+
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;
+ // Reference to the player GameObject.
+ public GameObject player;
 
-    private Vector3 offset;
+ // The distance between the camera and the player.
+ private Vector3 offset;
 
-    // Start is called before the first frame update
-    void Start()
+ // Start is called before the first frame update.
+ void Start()
     {
-        offset = transform.position - player.transform.position;
+ // Calculate the initial offset between the camera's position and the player's position.
+        offset = transform.position - player.transform.position; 
     }
 
-    // Update is called once per frame
-    void LateUpdate()
+ // LateUpdate is called once per frame after all Update functions have been completed.
+ void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+ // Maintain the same offset between the camera and player throughout the game.
+        transform.position = player.transform.position + offset;  
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,36 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0;
+ // Rigidbody of the player.
+ private Rigidbody rb; 
 
-    private Rigidbody rb;
+ // Movement along X and Y axes.
+ private float movementX;
+ private float movementY;
 
-    private float movementX;
-    private float movementY;
+ // Speed at which the player moves.
+ public float speed = 0; 
 
-    // Start is called before the first frame update
-    void Start()
+ // Start is called before the first frame update.
+ void Start()
     {
+ // Get and store the Rigidbody component attached to the player.
         rb = GetComponent<Rigidbody>();
     }
-
-    private void OnMove(InputValue movementValue)
+ 
+ // This function is called when a move input is detected.
+ void OnMove(InputValue movementValue)
     {
+ // Convert the input value into a Vector2 for movement.
         Vector2 movementVector = movementValue.Get<Vector2>();
 
-        movementX = movementVector.x;
-        movementY = movementVector.y;
+ // Store the X and Y components of the movement.
+        movementX = movementVector.x; 
+        movementY = movementVector.y; 
     }
 
-    private void FixedUpdate()
+ // FixedUpdate is called once per fixed frame-rate frame.
+ private void FixedUpdate() 
     {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+ // Create a 3D movement vector using the X and Y inputs.
+        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
 
-        rb.AddForce(movement * speed);
+ // Apply force to the Rigidbody to move the player.
+        rb.AddForce(movement * speed); 
     }
-
 }
